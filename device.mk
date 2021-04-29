e39fefdfe6be7dc5f2396a3c564ce5f549751d7b
ejbtrd <ejbtrd@gmail.com>
2021-04-29 19:47:42 +0000

    apollo: Cleanup overlays
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- device.mk
+++ device.mk
@@ -9,8 +9,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
