1b176521e7591b115c4cc0a0cb0fca18b625908a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:36:07 +0200

    apollo: Add libinit for variants handling
    Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,11 +17,15 @@ BUILD_BROKEN_DUP_RULES := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
+TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := apollo,apollopro,apollon
+TARGET_OTA_ASSERT_DEVICE := apollo,apollopro,apollo
 
 # Inherit from the proprietary version
 include vendor/xiaomi/apollo/BoardConfigVendor.mk
