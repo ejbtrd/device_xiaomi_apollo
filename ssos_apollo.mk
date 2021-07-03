401efd9616cd3b151e008897439d9bd1c14aa201
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-07-03 22:24:51 +0200

    apollo: ShapeShift-ify!

--- ssos_apollo.mk
+++ ssos_apollo.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ShapeShift stuff.
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := ssos_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
